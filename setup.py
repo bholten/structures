bdf3aa7806863141661e1e4d87dee5f7288d0861
Brennan Holten <brennan@telsasoft.com>
2019-01-09 10:45:46 -0600

    Update setup.py project name to keep pypi quiet.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='structures',
+    name='py_structures',
     version='0.0.1',
     packages=find_packages(),
     setup_requires=["pytest-runner"],
