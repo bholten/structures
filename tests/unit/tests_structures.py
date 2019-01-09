785f0fa3995b4661dd2d1173d678c33afbd36db5
Brennan Holten <brennan@telsasoft.com>
2019-01-08 19:54:43 -0600

    Tests

---
+++ tests/unit/tests_structures.py
@@ -0,0 +1,12 @@
+# Basic testing
+from structures import structure
+
+
+def test_basic_structures():
+    foo = structure(a=1, b=2, c=3, d=4)
+    assert foo.a == 1
+    assert foo.b == 2
+    assert foo.c == 3
+    assert foo.d == 4
+    assert foo.__slots__ is not None
+
