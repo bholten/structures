f2fc32104c4d312bee775ad1365a561b15f250f2
Brennan Holten <36414072+bholten@users.noreply.github.com>
2019-01-16 11:40:58 -0600

    General fixes (#2)
    * General cleanup and documentation fixes.
    
    * Updated travis

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py-structures',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
