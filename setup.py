facbca79dc9b41d2de898c775b4fd0112fdcdb01
Brennan Holten <brennan@telsasoft.com>
2019-01-16 11:50:06 -0600

    Attempting to update travis for pypi builds

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py-structures',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
